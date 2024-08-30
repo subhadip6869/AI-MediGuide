7dcfd75cc2e1aec7d902e1d482e85930330bce2c
Subhadip Dutta <subhadip.dutta@quickelf.com>
2024-08-30 18:59:33 +0530

    initialized project with html files

---
+++ src/index.js
@@ -0,0 +1,26 @@
+require("dotenv").config();
+
+const PORT = process.env.PORT || 3000;
+const HOST = process.env.HOSTNAME || "http://127.0.0.1";
+
+const path = require("path");
+const hbs = require("hbs");
+const express = require("express");
+const app = express();
+
+const webRouter = require("./routers/webrouter");
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(express.static(path.join(__dirname, "static")));
+app.set("view engine", "hbs");
+app.set("views", path.join(__dirname, "views"));
+
+hbs.registerPartials(path.join(__dirname, 'views', 'partials'));
+
+app.use("/", webRouter);
+
+app.listen(PORT, () => {
+    console.log(`Server running at ${HOST}:${PORT}`);
+});
+
