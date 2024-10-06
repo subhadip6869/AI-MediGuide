515d944d72f12ba2296fd5d7795e611dddef6357
Subhadip Dutta <subhadip3105@gmail.com>
2024-10-06 17:20:01 +0530

    added details viewing of search results

--- src/static/js/search.js
+++ src/static/js/search.js
@@ -8,20 +8,130 @@ const protocol = window.location.protocol;
 
 console.log(protocol, host, port);
 
-function createResultContainer({ name, manufacturer, prescriptionRequired, imageUrl }) {
+function onDetailsClick({ name,
+	manufacturer,
+	productForm,
+	ingredients,
+	usage,
+	sideEffect,
+	precaution,
+	category,
+	prescriptionRequired,
+	productUrls,
+	imageUrl,
+	invalidMessage }) {
+	showModalDetails({
+		element: createDetailsResponse({
+			name,
+			manufacturer,
+			productForm,
+			ingredients,
+			usage,
+			sideEffect,
+			precaution,
+			category,
+			prescriptionRequired,
+			productUrls,
+			imageUrl,
+			invalidMessage
+		})
+	});
+}
+
+function createResultContainer({ name,
+	manufacturer,
+	productForm,
+	ingredients,
+	usage,
+	sideEffect,
+	precaution,
+	category,
+	prescriptionRequired,
+	productUrls,
+	imageUrl,
+	invalidMessage }) {
 	return `<div
 				class="results-container flex items-center justify-between border-2 border-gray-100 rounded-xl p-2 my-1">
 				<div class="infos flex flex-col">
 					<p class="text-sm text-gray-400">${prescriptionRequired ? "Rx" : "OTC"}</p>
 					<p class="text-lg font-bold">${name}</p>
 					<p class="text-sm text-gray-400">Manufacturer: ${manufacturer}</p>
-					<button class="bg-gray-100 rounded-xl px-3 py-2 w-fit text-sm mt-2">View Details</button>
+					<button onClick="onDetailsClick({
+						name: '${name}', 
+						manufacturer: '${manufacturer}', 
+						productForm: '${productForm}', 
+						ingredients: '${ingredients.join(", ")}', 
+						usage: '${usage}', 
+						sideEffect: '${sideEffect}', 
+						precaution: '${precaution}', 
+						category: '${category}', 
+						prescriptionRequired: ${prescriptionRequired}
+					})" 
+					id="view-details" 
+					class="bg-gray-100 rounded-xl px-3 py-2 w-fit text-sm mt-2"
+				>
+					View Details
+				</button>
 				</div>
 				<div class="image">
 					<img src="${imageUrl ? imageUrl : '/images/default.jpg'}" class="w-40" alt="">
 				</div>
 			</div>`;
 }
+
+function createDetailsSection({ title, description }) {
+	return `<div class="section my-2">
+				<p class="font-bold">${title}</p>
+				<p>${description}</p>
+			</div>`;
+}
+
+function createDetailsResponse({ name, manufacturer, productForm, ingredients, usage, sideEffect, precaution, category, prescriptionRequired, productUrls, imageUrl, invalidMessage }) {
+	return `<div class="flex flex-col">
+				<h1 class="font-bold text-3xl">${name}</h1>
+
+                ${createDetailsSection({ title: "Manufacturer", description: manufacturer })}
+
+                ${createDetailsSection({ title: "Product Form", description: productForm })}
+
+				${createDetailsSection({ title: "Ingredients", description: ingredients })}
+
+				${createDetailsSection({ title: "Usage", description: usage })}
+
+				${createDetailsSection({ title: "Side Effects", description: sideEffect })}
+				
+				${createDetailsSection({ title: "Precaution", description: precaution })}
+
+				${createDetailsSection({ title: "Category", description: category })}
+
+				${createDetailsSection({ title: "Is this medicine prescription or over the counter?", description: prescriptionRequired ? "Rx" : "OTC" })}
+			</div>`;
+}
+
+function showModal({ message }) {
+	$("#myModal .modal-details").empty();
+	$("#myModal .content").text(message);
+	$("#myModal").fadeIn(300);
+}
+
+function showModalDetails({ element }) {
+	$("#myModal .content").text("");
+	$("#myModal .modal-details").empty();
+	$("#myModal .modal-details").append(element);
+	$("#myModal").fadeIn(300);
+}
+
+function hideModal() {
+	$("#myModal .content").text("");
+	$("#myModal .modal-details").empty();
+	$("#myModal").fadeOut(300);
+}
+
+$("#myModal").hide(0);
+
+$(".close").click(() => {
+	hideModal();
+});
 
 $(document).ready(() => {
 	$('.loader-container').hide();
@@ -30,7 +140,7 @@ $(document).ready(() => {
 
 		if (inputText) {
 			console.log(`Searching with: ${inputText}`);
-			$('.loader-container').show(500);
+			$('.loader-container').fadeIn(200);
 
 			try {
 				let count = 10;
@@ -58,8 +168,16 @@ $(document).ready(() => {
 							$(".results").append(createResultContainer({
 								name: data[i].NAME,
 								manufacturer: data[i].MANUFACTURER,
+								productForm: data[i].PRODUCT_FORM,
+								ingredients: data[i].INGREDIENT,
+								usage: data[i].USAGE,
+								sideEffect: data[i].SIDE_EFFECT,
+								precaution: data[i].PRECAUTION,
+								category: data[i].CATEGORY,
 								prescriptionRequired: data[i].PRESCRIPTION_REQUIRED,
-								imageUrl: ""
+								productUrls: data[i].PRODUCT_URL,
+								imageUrl: "",
+								invalidMessage: data[i].INVALID_MESSAGE,
 							}));
 						}
 						break;
@@ -68,138 +186,13 @@ $(document).ready(() => {
 				}
 
 				if (count === 0) {
-					alert("No results found");
+					showModal({ message: "No results found" });
 				}
 			} catch (e) {
-				console.log(e.message);
+				showModal({ message: "Something went wrong" });
 			} finally {
-				$('.loader-container').hide(500);
+				$('.loader-container').fadeOut(200);
 			}
 		}
 	});
 });
-
-// function createElementWithText(tag, text) {
-// 	const element = document.createElement(tag);
-// 	element.innerHTML = text;
-// 	return element;
-// }
-
-// findBtn.addEventListener("click", async () => {
-// 	let counter = 0;
-// 	if (!inputField.value) return;
-// 	const errorMsgElem = document.querySelector("#medicineOutputModal p");
-// 	errorMsgElem.style.display = "none";
-// 	btnLoader.style.display = "block";
-// 	btnText.style.display = "none";
-// 	while (true) {
-// 		try {
-// 			let res = await fetch(`${protocol}//${host}:${port}/api/info`, {
-// 				method: "POST",
-// 				headers: {
-// 					"Content-Type": "application/json",
-// 				},
-// 				body: JSON.stringify({
-// 					medicines: inputField.value,
-// 				}),
-// 			});
-
-// 			let responseData = await res.json();
-
-// 			if (responseData.status == 200) {
-// 				const modalContainer = document.getElementById(
-// 					"medicineOutputModal"
-// 				);
-
-// 				const table = document.querySelector(
-// 					"#medicineOutputModal table"
-// 				);
-// 				table.replaceChildren();
-
-// 				// adding table headers
-// 				let headerRow = document.createElement("tr");
-// 				headerRow.classList.add("bg-slate-300");
-
-// 				// adding heading labels
-// 				for (
-// 					let i = 0;
-// 					i < Object.keys(responseData.data[0]).length - 1;
-// 					i++
-// 				) {
-// 					let th = createElementWithText(
-// 						"th",
-// 						Object.keys(responseData.data[0])[i].replace("_", " ")
-// 					);
-// 					th.classList.add("p-2.5", "border", "border-slate-700");
-// 					headerRow.appendChild(th);
-// 				}
-
-// 				table.appendChild(headerRow);
-// 				// end of adding table headers
-
-// 				let data = responseData.data;
-
-// 				// adding output data
-// 				for (let i = 0; i < data.length; i++) {
-// 					if (data[i]["INVALID_MESSAGE"]) {
-// 						continue;
-// 					}
-
-// 					const trow = document.createElement("tr");
-
-// 					let keys = Object.keys(data[i]);
-// 					for (let j = 0; j < keys.length - 1; j++) {
-// 						if (keys[j] === "PRESCRIPTION_REQUIRED") {
-// 							let td = createElementWithText(
-// 								"td",
-// 								data[i][keys[j]] ? "Rx" : "OTC"
-// 							);
-// 							td.classList.add(
-// 								"p-2.5",
-// 								"border",
-// 								"border-slate-700"
-// 							);
-// 							trow.appendChild(td);
-// 						} else {
-// 							let td = createElementWithText(
-// 								"td",
-// 								data[i][keys[j]]
-// 							);
-// 							td.classList.add(
-// 								"p-2.5",
-// 								"border",
-// 								"border-slate-700"
-// 							);
-// 							trow.appendChild(td);
-// 						}
-// 					}
-
-// 					table.appendChild(trow);
-// 				}
-
-// 				let modal = new bootstrap.Modal(modalContainer);
-// 				modal.show();
-// 				break;
-// 			} else {
-// 				console.log(responseData);
-// 				if (++counter == 10) {
-// 					document
-// 						.querySelector("#medicineOutputModal table")
-// 						.replaceChildren();
-// 					errorMsgElem.style.display = "block";
-// 					let modal = new bootstrap.Modal(
-// 						document.getElementById("medicineOutputModal")
-// 					);
-// 					modal.show();
-// 					break;
-// 				}
-// 				// alert(responseData.message);
-// 			}
-// 		} catch (e) {
-// 			console.log(e);
-// 		} finally {
-// 			btnText.style.display = "block";
-// 			btnLoader.style.display = "none";
-// 		}
-// 	}
-// });
