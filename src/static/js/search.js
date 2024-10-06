7277fccfafa4e9bbc78f723923e6ab506547d5aa
Subhadip Dutta <subhadip3105@gmail.com>
2024-10-06 22:05:23 +0530

    added landing image, added recent searches, added product URLs

--- src/static/js/search.js
+++ src/static/js/search.js
@@ -1,13 +1,47 @@
-// const findBtn = document.querySelector("#search");
-// const inputField = document.querySelector("#textfield");
-// const btnText = document.querySelector(".btn-text");
-// const btnLoader = document.querySelector(".loader");
 const host = window.location.hostname;
 const port = window.location.port;
 const protocol = window.location.protocol;
 
 console.log(protocol, host, port);
 
+// Links container
+function createLinksContainer({ url, title }) {
+	return `<a href="${url}" target="_blank" class="text-blue-500 hover:text-blue-700 hover:underline">${title ? title : url}</a>`
+}
+
+// details view inside the modal
+function createDetailsResponse({ name, manufacturer, productForm, ingredients, usage, sideEffect, precaution, category, prescriptionRequired, productUrls, imageUrl, invalidMessage }) {
+	return `<div class="flex flex-col">
+				<h1 class="font-bold text-3xl">${name}</h1>
+
+                ${createDetailsSection({ title: "Manufacturer", description: manufacturer })}
+
+                ${createDetailsSection({ title: "Product Form", description: productForm })}
+
+				${createDetailsSection({ title: "Ingredients", description: ingredients })}
+
+				${createDetailsSection({ title: "Usage", description: usage })}
+
+				${createDetailsSection({ title: "Side Effects", description: sideEffect })}
+				
+				${createDetailsSection({ title: "Precaution", description: precaution })}
+
+				${createDetailsSection({ title: "Category", description: category })}
+
+				${createDetailsSection({ title: "Is this medicine prescription or over the counter?", description: prescriptionRequired === true ? "Rx" : prescriptionRequired === false ? "OTC" : "Not available" })}
+
+				<div class="section my-2">
+					<p class="font-bold">Where I can order?</p>
+					<div class="flex flex-wrap gap-2">
+					${productUrls.length > 0
+			? productUrls.map(url => createLinksContainer({ url: url, title: new URL(url).hostname })).join('')
+			: "Not available"}
+					</div>
+				</div>
+			</div>`;
+}
+
+// runs when "View Details" button is clicked, it passes the necessary data and generates the details view for the model
 function onDetailsClick({ name,
 	manufacturer,
 	productForm,
@@ -31,13 +65,14 @@ function onDetailsClick({ name,
 			precaution,
 			category,
 			prescriptionRequired,
-			productUrls,
+			productUrls: productUrls.split(","),
 			imageUrl,
 			invalidMessage
 		})
 	});
 }
 
+// medicine overview cards after search is complete
 function createResultContainer({ name,
 	manufacturer,
 	productForm,
@@ -55,23 +90,24 @@ function createResultContainer({ name,
 				<div class="infos flex flex-col">
 					<p class="text-sm text-gray-400">${prescriptionRequired ? "Rx" : "OTC"}</p>
 					<p class="text-lg font-bold">${name}</p>
-					<p class="text-sm text-gray-400">Manufacturer: ${manufacturer}</p>
+					<p class="text-sm text-gray-400">Manufacturer: ${manufacturer ? manufacturer : "N/A"}</p>
 					<button onClick="onDetailsClick({
-						name: '${name}', 
-						manufacturer: '${manufacturer}', 
-						productForm: '${productForm}', 
-						ingredients: '${ingredients.join(", ")}', 
-						usage: '${usage}', 
-						sideEffect: '${sideEffect}', 
-						precaution: '${precaution}', 
-						category: '${category}', 
-						prescriptionRequired: ${prescriptionRequired}
-					})" 
-					id="view-details" 
-					class="bg-gray-100 rounded-xl px-3 py-2 w-fit text-sm mt-2"
-				>
-					View Details
-				</button>
+						name: '${name.replace(/'/g, "\\'")}', 
+						manufacturer: '${manufacturer.replace(/'/g, "\\'")}', 
+						productForm: '${productForm.replace(/'/g, "\\'")}', 
+						ingredients: '${ingredients.join(", ").replace(/'/g, "\\'")}', 
+						usage: '${usage.replace(/'/g, "\\'")}', 
+						sideEffect: '${sideEffect.replace(/'/g, "\\'")}', 
+						precaution: '${precaution.replace(/'/g, "\\'")}', 
+						category: '${category.replace(/'/g, "\\'")}', 
+						prescriptionRequired: ${prescriptionRequired},
+						productUrls: '${productUrls.join(",")}',
+						})" 
+						id="view-details" 
+						class="bg-gray-100 rounded-xl px-3 py-2 w-fit text-sm mt-2"
+					>
+						View Details
+					</button>
 				</div>
 				<div class="image">
 					<img src="${imageUrl ? imageUrl : '/images/default.jpg'}" class="w-40" alt="">
@@ -79,41 +115,22 @@ function createResultContainer({ name,
 			</div>`;
 }
 
+// each section for the details view inside the modal
 function createDetailsSection({ title, description }) {
 	return `<div class="section my-2">
 				<p class="font-bold">${title}</p>
-				<p>${description}</p>
+				<p>${description ? description : "Not available"}</p>
 			</div>`;
 }
 
-function createDetailsResponse({ name, manufacturer, productForm, ingredients, usage, sideEffect, precaution, category, prescriptionRequired, productUrls, imageUrl, invalidMessage }) {
-	return `<div class="flex flex-col">
-				<h1 class="font-bold text-3xl">${name}</h1>
-
-                ${createDetailsSection({ title: "Manufacturer", description: manufacturer })}
-
-                ${createDetailsSection({ title: "Product Form", description: productForm })}
-
-				${createDetailsSection({ title: "Ingredients", description: ingredients })}
-
-				${createDetailsSection({ title: "Usage", description: usage })}
-
-				${createDetailsSection({ title: "Side Effects", description: sideEffect })}
-				
-				${createDetailsSection({ title: "Precaution", description: precaution })}
-
-				${createDetailsSection({ title: "Category", description: category })}
-
-				${createDetailsSection({ title: "Is this medicine prescription or over the counter?", description: prescriptionRequired ? "Rx" : "OTC" })}
-			</div>`;
-}
-
+// helps to show the modal by taking a simple message
 function showModal({ message }) {
 	$("#myModal .modal-details").empty();
 	$("#myModal .content").text(message);
 	$("#myModal").fadeIn(300);
 }
 
+// shows the modal by taking and whole html element
 function showModalDetails({ element }) {
 	$("#myModal .content").text("");
 	$("#myModal .modal-details").empty();
@@ -121,25 +138,81 @@ function showModalDetails({ element }) {
 	$("#myModal").fadeIn(300);
 }
 
+// hides & clears modal data
 function hideModal() {
 	$("#myModal .content").text("");
 	$("#myModal .modal-details").empty();
 	$("#myModal").fadeOut(300);
 }
 
+
+// hides modal when page is loading for first time
 $("#myModal").hide(0);
 
+// close modal on clicking on cross button inside modal
 $(".close").click(() => {
 	hideModal();
 });
 
+// inserting upto 5 recent search keywords into local storage
+function storeRecentSearches({ keyword }) {
+	let recentSearches = localStorage.getItem("recentSearches");
+	if (recentSearches) {
+		recentSearches = JSON.parse(recentSearches);
+		if (recentSearches.length >= 5) {
+			recentSearches.pop();
+		}
+	} else {
+		recentSearches = [];
+	}
+	recentSearches.unshift(keyword);
+	recentSearches = [...new Set(recentSearches)];
+	localStorage.setItem("recentSearches", JSON.stringify(recentSearches));
+}
+
+// fetching stored recent searches
+function getRecentSearches() {
+	let recentSearches = localStorage.getItem("recentSearches");
+	if (recentSearches) {
+		recentSearches = JSON.parse(recentSearches);
+	} else {
+		recentSearches = [];
+	}
+	return recentSearches;
+}
+
+// recent search keyword pills
+function createRecentSearchPill({ keyword }) {
+	return `<button class="bg-gray-100 rounded-xl px-3 py-2 w-fit text-sm mt-2"
+					onClick="onRecentPillClick({ keyword: '${keyword}' })">
+				${keyword}
+			</button>`;
+}
+function onRecentPillClick({ keyword }) {
+	$("#textfield").val(keyword);
+	// $("#search").click();
+}
+
 $(document).ready(() => {
+
 	$('.loader-container').hide();
+	for (let key of getRecentSearches()) {
+		$(".recent-keywords").append(createRecentSearchPill({ keyword: key }));
+	}
+
 	$("#search").click(async () => {
 		let inputText = $("#textfield").val();
 
 		if (inputText) {
+			// refreshing the recent searches
+			storeRecentSearches({ keyword: inputText });
+			$(".recent-keywords").empty();
+			for (let key of getRecentSearches()) {
+				$(".recent-keywords").append(createRecentSearchPill({ keyword: key }));
+			}
+
 			console.log(`Searching with: ${inputText}`);
+			$('.landing-text').hide();
 			$('.loader-container').fadeIn(200);
 
 			try {
